d058f18e7cb6a09974ee3d87f9d63374e4d64b96
mnov88 <47901419+mnov88@users.noreply.github.com>
2023-09-12 14:23:20 +0200

    2 files modified

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,41 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+
       // Replace with your project's social card
+      typesense: {
+        // Replace this with the name of your index/collection.
+        // It should match the "index_name" entry in the scraper's "config.json" file.
+        typesenseCollectionName: 'docusaurus-2',
+  
+        typesenseServerConfig: {
+          nodes: [
+            {
+              host: 'xxx-1.a1.typesense.net',
+              port: 443,
+              protocol: 'https',
+            },
+            {
+              host: 'xxx-2.a1.typesense.net',
+              port: 443,
+              protocol: 'https',
+            },
+            {
+              host: 'xxx-3.a1.typesense.net',
+              port: 443,
+              protocol: 'https',
+            },
+          ],
+          apiKey: 'xyz',
+        },
+  
+        // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
+        typesenseSearchParameters: {},
+  
+        // Optional
+        contextualSearch: true,
+      },
+    
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: 'EU Digital Services Act',
